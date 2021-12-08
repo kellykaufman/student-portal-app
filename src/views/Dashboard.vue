db5ef97e53ac05844a8a9d02e69048de52c482d8
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-08 13:36:25 -0800

    Adds dashboard

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="students-dashboard">
+    <h1>{{ student.first_name + " " + student.last_name }}</h1>
+    <p>{{ message }}</p>
+
+    <button v-on:click="dashboardStudent(student)">Dashboard</button>
+    <dialog id="display student resume and capstone">
+      <form method="dialog">
+        <button>close</button>
+      </form>
+    </dialog>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+// import axios from "axios";
+export default {
+  data: function () {
+    return {
+      message: "Welcome to the student portal.",
+      student: {},
+    };
+  },
+  created: function () {},
+  methods: {
+    dashboardStudent: function () {
+      // document.querySelector("#edit-student-info").dashboardModal();
+    },
+  },
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,18 @@
 <template>
   <div class="home">
-    <p>{{ student }}</p>
+    <p>{{ message }}</p>
 
-    <button v-on:click="showStudent(student)">Show Student</button>
+    <button v-on:click="showStudent(student)">Dashboard</button>
     <dialog id="edit-student-info">
       <form method="dialog">
-        <h1>Student Information:</h1>
-        <p></p>
+        <!-- <h1>Student Information:</h1>
+        <p>
+          First Name: Jarret Last Name: Nachtigal Email: jn@example.com Phone
+          Number: 123-456-789 Short Bio: Jarret is the best! Linkedin Url:
+          linkedin.com/in/jarret-nachtigal-42a849220 Twitter Handle: N/A Website
+          Url:https://github.com/JarretNachtigal online_resume_url: N/A
+          github_url: https://github.com/JarretNachtigal photo: jarret.png
+        </p> -->
         <button>close</button>
       </form>
     </dialog>
@@ -20,29 +26,17 @@ import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
-      student: {
-        first_name: "Jarret",
-        last_name: "Nachtigal",
-        email: "jn@example.com",
-        phone_number: "123-456-789",
-        short_bio: "Jarret is the best!",
-        linkedin_url: "linkedin.com/in/jarret-nachtigal-42a849220",
-        twitter_handle: "N/A",
-        website_url: "https://github.com/JarretNachtigal",
-        online_resume_url: "N/A",
-        github_url: "https://github.com/JarretNachtigal",
-        photo: "jarret.png",
-      },
+      message: "Welcome to the student portal.",
+      student: {},
     };
   },
   created: function () {},
   methods: {
     showStudent: function () {
+      this.$router.push("/dashboard");
       // axios.get("/students").then((response) => {
       // this.student = response.data;
       // console.log("One student", this.student);
-      document.querySelector("#edit-student-info").showModal();
       // });
     },
     updateStudent: function (student) {
