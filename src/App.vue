75360f30d4d75eb74acf05667671e7fe1b2a57d1
Jarret Nachtigal <jarretn1224@gmail.com>
2021-12-08 15:19:54 -0500

    commented out login route in app.vue (not needed?)

--- src/App.vue
+++ src/App.vue
@@ -6,8 +6,8 @@
         <router-link to="/">Home</router-link>
         |
         <router-link to="/about">About</router-link>
-        |
-        <router-link to="/login">Login</router-link>
+        <!-- |
+        <router-link to="/login">Login</router-link> -->
         |
         <router-link to="/logout">Logout</router-link>
       </div>
