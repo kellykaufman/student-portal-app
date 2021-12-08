fe5d73e76a0b85ca6be8c3e409b0b5b37532ccba
Kelly Kaufman <kellykaufman12@gmail.com>
2021-12-08 11:07:15 -0800

    adds close button and show student button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,54 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
+    <p>{{ student }}</p>
+
+    <button v-on:click="showStudent(student)">Show Student</button>
+    <dialog id="edit-student-info">
+      <form method="dialog">
+        <h1>Student Information:</h1>
+        <p></p>
+        <button>close</button>
+      </form>
+    </dialog>
   </div>
 </template>
 
 <style></style>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to Vue.js!",
-      students: [],
+      student: {
+        first_name: "Jarret",
+        last_name: "Nachtigal",
+        email: "jn@example.com",
+        phone_number: "123-456-789",
+        short_bio: "Jarret is the best!",
+        linkedin_url: "linkedin.com/in/jarret-nachtigal-42a849220",
+        twitter_handle: "N/A",
+        website_url: "https://github.com/JarretNachtigal",
+        online_resume_url: "N/A",
+        github_url: "https://github.com/JarretNachtigal",
+        photo: "jarret.png",
+      },
     };
   },
   created: function () {},
   methods: {
-    indexStudents: function () {
-      axios.get();
+    showStudent: function () {
+      // axios.get("/students").then((response) => {
+      // this.student = response.data;
+      // console.log("One student", this.student);
+      document.querySelector("#edit-student-info").showModal();
+      // });
+    },
+    updateStudent: function (student) {
+      axios.patch("/students/" + student.id, student).then((response) => {
+        console.log("Success", response.data);
+      });
     },
   },
 };
