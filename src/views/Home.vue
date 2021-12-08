9e967f111c717ddfce621828b7789da27940320a
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-08 13:52:50 -0600

    adds html elements

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="home">
-    <p>{{ student }}</p>
-
-    <button v-on:click="showStudent(student)">Show Student</button>
+    <img :src="student.photo" :alt="student.first_name" />
+    <h1>{{ student.first_name }} {{ student.last_name }}</h1>
+    <button v-on:click="showStudent(student)">Contact Info</button>
     <dialog id="edit-student-info">
       <form method="dialog">
-        <h1>Student Information:</h1>
-        <p></p>
+        <h1>Contact Info:</h1>
+        <h3>Email:</h3>
+        <p>{{ student.email }}</p>
+        <h3>Phone Number:</h3>
+        <p>{{ student.phone_number }}</p>
         <button>close</button>
       </form>
     </dialog>
+    <h2>About</h2>
+    <p>{{ student.short_bio }}</p>
+    <h2>Experience</h2>
+    <h2>Education</h2>
+    <h2>Skills</h2>
+    <h2>Capstone</h2>
   </div>
 </template>
 
