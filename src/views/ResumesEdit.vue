573b5ef94003d061dd37022d23b9ec580809a5e3
lucastimho <lucas.t.ho.lh@gmail.com>
2021-12-09 14:08:33 -0600

    completes first draft of ResumesEdit.vue

--- src/views/ResumesEdit.vue
+++ src/views/ResumesEdit.vue
@@ -1,80 +1,186 @@
 <template>
-  <div>
-    <h1>{{ student.first_name }} {{ student.last_name }}</h1>
-    <h2>Experience:</h2>
-    <div v-for="experience in experiences" :key="experience.id">
-      <p>{{ experience }}</p>
+  <div class="resume-edit">
+    <div v-for="experience in currentExperienceParams" :key="experience.id">
+      <form v-on:submit.prevent="updateExperience()">
+        <h1>Editing Experience</h1>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label>Start Date:</label>
+          <textarea name="input" cols="10" rows="1" v-model="experience.start_date"></textarea>
+        </div>
+        <div>
+          <label>End Date:</label>
+          <textarea name="input" cols="10" rows="1" v-model="experience.end_date"></textarea>
+        </div>
+        <div>
+          <label>Job Title:</label>
+          <textarea name="input" cols="10" rows="1" v-model="experience.job_title"></textarea>
+        </div>
+        <div>
+          <label>Company:</label>
+          <textarea name="input" cols="10" rows="1" v-model="experience.company"></textarea>
+        </div>
+        <div>
+          <label>Details:</label>
+          <textarea name="input" cols="30" rows="5" v-model="experience.detail"></textarea>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      <button v-on:click="deleteExperience()">Delete</button>
     </div>
-    <h2>Education:</h2>
-    <div v-for="education in educations" :key="education.id">
-      <p>{{ education }}</p>
+    <div v-for="education in currentEducationParams" :key="education.id">
+      <form v-on:submit.prevent="updateEducation()">
+        <h1>Editing Education</h1>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label>Start Date:</label>
+          <textarea name="input" cols="10" rows="1" v-model="education.start_date"></textarea>
+        </div>
+        <div>
+          <label>End Date:</label>
+          <textarea name="input" cols="10" rows="1" v-model="education.end_date"></textarea>
+        </div>
+        <div>
+          <label>Degree:</label>
+          <textarea name="input" cols="10" rows="1" v-model="education.degree"></textarea>
+        </div>
+        <div>
+          <label>University:</label>
+          <textarea name="input" cols="10" rows="1" v-model="education.university_name"></textarea>
+        </div>
+        <div>
+          <label>Details:</label>
+          <textarea name="input" cols="30" rows="5" v-model="education.detail"></textarea>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      <button v-on:click="deleteEducation()">Delete</button>
     </div>
-    <h2>Skills:</h2>
-    <p>{{ skills }}</p>
+    <div v-for="skill in currentSkillParams" :key="skill.id">
+      <form v-on:submit.prevent="updateSkills()">
+        <h1>Editing Skills</h1>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label>Skill:</label>
+          <textarea name="input" cols="10" rows="1" v-model="skill.skill"></textarea>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      <button v-on:click="deleteExperience()">Delete</button>
+    </div>
+    <div v-for="capstone in currentCapstoneParams" :key="capstone.id">
+      <form v-on:submit.prevent="updateCapstone()">
+        <h1>Editing Capstone</h1>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label>Name:</label>
+          <textarea name="input" cols="10" rows="1" v-model="capstone.name"></textarea>
+        </div>
+        <div>
+          <label>Description:</label>
+          <textarea name="input" cols="30" rows="10" v-model="capstone.description"></textarea>
+        </div>
+        <div>
+          <label>URL:</label>
+          <textarea name="input" cols="10" rows="1" v-model="capstone.url"></textarea>
+        </div>
+        <div>
+          <label>Screenshot:</label>
+          <textarea name="input" cols="10" rows="1" v-model="capstone.screenshot"></textarea>
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      <button v-on:click="deleteExperience()">Delete</button>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      student: [
-        {
-          id: 1,
-          first_name: "Jarret",
-          last_name: "Nachtigal",
-          email: "jn@example.com",
-          phone_number: "123-456-789",
-          short_bio: "Jarret is the best!",
-          linkedin_url: "linkedin.com/in/jarret-nachtigal-42a849220",
-          twitter_handle: "N/A",
-          website_url: "https://github.com/JarretNachtigal",
-          online_resume_url: "N/A",
-          github_url: "https://github.com/JarretNachtigal",
-          photo: "jarret.png",
-        },
-      ],
-      experiences: [
-        {
-          id: 2,
-          start_date: "12-12-12",
-          end_date: "12-12-16",
-          job_title: "President",
-          company: "US Government",
-          details:
-            "Oversaw the entirety of the United States of America. It was a pretty easy task. No need to thank me, did it for the love of the sport.",
-        },
-        {
-          id: 1,
-          start_date: "12-12-09",
-          end_date: "10-12-11",
-          job_title: "Space Diver",
-          company: "NASA",
-          details:
-            "Launched the entirety of the SS Apollo out of the gravitational force of the Earth's orbit. Viewed the Earth in ways that 99% will never get to experience.",
-        },
-      ],
-      educations: [
-        {
-          id: 1,
-          start_date: "01-01-00",
-          end_date: "01-02-00",
-          degree: "Cryptocurrency",
-          university_name: "The University of Neptune",
-          details:
-            "Obtained PhD in cryptocurrency after obtaining a bachelors in Egyptology and interning with the CIA and Atlantis Turtle Academy.",
-        },
-      ],
-      skills: ["X-ray vision", "Pickpocketing", "CSS", "Tomahawk Throwing", "Pyramid Scheming", "English"],
-      capstones: {
-        name: "Complete Automation",
-        description:
-          "Created a bot that automates all aspect of society so that you can sit at home and watch YouTube vidoes.",
-        url: "reddit.com",
-        screenshot: "actualize.png",
-      },
-      studentPostParams: {},
+      currentExperienceParams: {},
+      currentEducationParams: {},
+      currentSkillParams: {},
+      currentCapstoneParams: {},
     };
+  },
+  created: function () {
+    axios.get(`/experience/${this.$route.params.id}`).then((response) => {
+      this.currentExperienceParams = response.data;
+    });
+    axios.get(`/education/${this.$route.params.id}`).then((response) => {
+      this.currentExperienceParams = response.data;
+    });
+    axios.get(`/skills/${this.$route.params.id}`).then((response) => {
+      this.currentExperienceParams = response.data;
+    });
+    axios.get(`/capstones/${this.$route.params.id}`).then((response) => {
+      this.currentExperienceParams = response.data;
+    });
+  },
+  methods: {
+    updateExperience: function () {
+      axios
+        .patch(`/experience/${this.$route.params.id}`)
+        .then((response) => {
+          this.$router.push(`/experience/${response.data.id}`);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    updateEducation: function () {
+      axios
+        .patch(`/experience/${this.$route.params.id}`)
+        .then((response) => {
+          this.$router.push(`/experience/${response.data.id}`);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    updateSkills: function () {
+      axios
+        .patch(`/experience/${this.$route.params.id}`)
+        .then((response) => {
+          this.$router.push(`/experience/${response.data.id}`);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    updateCapstone: function () {
+      axios
+        .patch(`/experience/${this.$route.params.id}`)
+        .then((response) => {
+          this.$router.push(`/experience/${response.data.id}`);
+        })
+        .catch((error) => console.log(error.response));
+    },
+  },
+  deleteExperience: function () {
+    axios.delete(`/experience/${this.$route.params.id}`).then((response) => {
+      console.log(response.data);
+    });
+  },
+  deleteEducation: function () {
+    axios.delete(`/experience/${this.$route.params.id}`).then((response) => {
+      console.log(response.data);
+    });
+  },
+  deleteSkill: function () {
+    axios.delete(`/experience/${this.$route.params.id}`).then((response) => {
+      console.log(response.data);
+    });
+  },
+  deleteCapstone: function () {
+    axios.delete(`/experience/${this.$route.params.id}`).then((response) => {
+      console.log(response.data);
+    });
   },
 };
 </script>
